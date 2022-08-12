52b6b41c4f50c65bf605b1af31a87b19ea82846a
Marco Sousa <msousa.fp@gmail.com>
2022-08-12 12:18:34 +0100

    Joke App completed

---
+++ src/main/java/com/mpsousa/jokes/controllers/JokesController.java
@@ -0,0 +1,23 @@
+package com.mpsousa.jokes.controllers;
+
+import com.mpsousa.jokes.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokesController {
+
+    private final JokeService jokeService;
+
+    public JokesController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";
+    }
+}
---
+++ src/main/java/com/mpsousa/jokes/services/JokeService.java
@@ -0,0 +1,5 @@
+package com.mpsousa.jokes.services;
+
+public interface JokeService {
+    String getJoke();
+}
